9a63f196052d4ffe61401dd6fcac5dd7e4c937a4
dbshun <1205278074@qq.com>
2020-03-25 11:21:41 +0800

    ff

---
+++ Fibonacci.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 25 09:50:24 2020
+
+@author: 12052
+"""
+def Fibonacci_of(num):
+    if num ==1:
+        return 1
+    if num == 2:
+        return 1
+    return Fibonacci_of(num-1)+Fibonacci_of(num-2)
+def main():
+    for i in range(1,21):
+        result=Fibonacci_of(i)
+        print(result)
+if __name__ == '__main__':
+    main()
